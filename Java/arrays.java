070b7ff7049735e848cb8e77bcb302368a7f0165
Rodrigo Castilho <rodrigo.m.castilho@outlook.com>
2020-07-18 17:49:37 -0300

    Adding arrays file

---
+++ Java/arrays.java
@@ -0,0 +1,15 @@
+public class arrays {
+    public static void main(String[] args){
+        // creating a list of strings
+        String[] inventory = {"Knife", "Bread", "Butter"};
+        String knife = inventory[0];
+        // replacing the bread
+        inventory[1] = "Fruit";
+        // trying to access an index that does not exist,
+        // such as inventory[2] will cause the program to crash
+
+        //creating an empty string array with 3 positions
+
+        String[] inventory2 = new String[3];
+    }
+}
