2f28585cb64774ee996b095b8c68ea8f3807e9ef
Rodrigo Castilho <rodrigo.m.castilho@outlook.com>
2020-07-18 18:43:18 -0300

    Adding while loops

---
+++ Java/whileLoops.java
@@ -0,0 +1,11 @@
+public class whileLoops {
+    public static void main(String[] args){
+        int position = 0;
+        int endPosition = 5;
+
+        while(position < endPosition){
+            position += 1;
+            System.out.println("Running");
+        }
+    }
+}
