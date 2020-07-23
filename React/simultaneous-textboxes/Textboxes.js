948139dbc6e3b8721ac86301291f2dc65248000a
Rodrigo Castilho <rodrigo.m.castilho@outlook.com>
2020-07-23 13:22:21 -0300

    Refactoring code for simplicity

--- React/simultaneous-textboxes/Textboxes.js
+++ React/simultaneous-textboxes/Textboxes.js
@@ -1,64 +1,43 @@
 import React, { Component } from 'react';
 
-class TextOne extends Component {
+class TextBoxes extends Component {
     constructor(props){
         super(props);
 
         this.state = {
-            textContent: null
+            textOne: "",
+            textTwo: ""
         }
-
-        this.toggleContent = this.toggleContent.bind(this);
-        this.toggleSelfContent = this.toggleSelfContent.bind(this);
     }
 
-    toggleContent(text){
-        null
-    }
-
-    toggleSelfContent(text){
-        this.setState(
-            { textContent: text }
-        );
+    handleChange(changeEvent) {
+        const value = changeEvent.target.value;
+        
+        this.setState({
+          ...state,
+          [ changeEvent.target.name ]: value
+        });
     }
 
     render(){
         return(
-            <input className="regular-box" id="text-one" value={ this.state.textContent } />
-        )
+            <form>
+                <label>Text one</label>
+                <input
+                    type="text"
+                    name="text-one"
+                    value={ state.textOne }
+                    onChange={ handleChange }
+                />
+
+                <label>Text two</label>
+                <input
+                    type="text"
+                    name="text-two"
+                    value={ state.textTwo }
+                    onChange={ handleChange }
+                />
+            </form>
+       );
     }
-}
-
-class TextTwo extends Component {
-    constructor(props){
-        super(props);
-
-        this.state = {
-            textContent: null
-        }
-
-        this.toggleContent = this.toggleContent.bind(this);
-        this.toggleSelfContent = this.toggleSelfContent.bind(this);
-    }
-
-    toggleContent(text){
-        null
-    }
-
-    toggleSelfContent(text){
-        this.setState(
-            { textContent: text }
-        );
-    }
-
-    render(){
-        return(
-            <input className="regular-box" id="text-two" onChange={ this.toggleContent() } value={ this.state.textContent } />
-        )
-    }
-}
-
-export {
-    TextOne,
-    TextTwo
 }
