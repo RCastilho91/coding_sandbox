84fda164b80d0b1bee0fc5d0c00e4624af75bbe5
Rodrigo Castilho <rodrigo.m.castilho@outlook.com>
2020-08-01 14:38:13 -0300

    Finished draggable div function

--- react-sandbox/src/components/draggable-divs/Draggable.js
+++ react-sandbox/src/components/draggable-divs/Draggable.js
@@ -5,17 +5,16 @@ class DraggableDiv extends Component {
     constructor(props){
         super(props);
         this.state = {
-            xCoordinate: 50,
-            yCoordinate: 50
+            xCoordinate: 150,
+            yCoordinate: 150
         }
     
         this.repositionDiv = this.repositionDiv.bind(this);
-    
     }
 
     repositionDiv(evt) {
-
         evt.preventDefault();
+
         evt = evt || window.event;
         var x = evt.pageX;
         var y = evt.pageY;
@@ -26,19 +25,18 @@ class DraggableDiv extends Component {
         });
 
         console.log(`X: ${x}, Y: ${y}`);
-        console.log(`State X: ${ this.state.xCoordinate }`);
-        console.log(`State Y: ${ this.state.yCoordinate }`)
+        console.log(`State X: ${ this.state.xCoordinate }, State Y: ${ this.state.yCoordinate }`);
     }
 
     render(){
         var style = {
-            "left": this.state.xCoordinate + "px",
-            "top": this.state.yCoordinate + "px",
+            "left": `${ this.state.xCoordinate }px`,
+            "top": `${ this.state.yCoordinate }px`,
             "background-color": "red"
         }
         
         return(
-            <div className="draggable-div" draggable onDrag={ (evt) => this.repositionDiv(evt) } style={ style }>
+            <div className="draggable-div" draggable onDragEnd={ (evt) => this.repositionDiv(evt) } style={ style }>
                 Drag me around!
             </div>
         )
