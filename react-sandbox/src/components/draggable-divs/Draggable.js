fb3b1c46f9455a06f81af7f032c9f0901af22a2c
Rodrigo Castilho <rodrigo.m.castilho@outlook.com>
2020-08-01 14:04:10 -0300

    Adding nearly all functionalities for a draggable div

--- react-sandbox/src/components/draggable-divs/Draggable.js
+++ react-sandbox/src/components/draggable-divs/Draggable.js
@@ -5,29 +5,40 @@ class DraggableDiv extends Component {
     constructor(props){
         super(props);
         this.state = {
-            xCoordinate: 0,
-            yCoordinate: 0
+            xCoordinate: 50,
+            yCoordinate: 50
         }
     
         this.repositionDiv = this.repositionDiv.bind(this);
     
     }
 
-    repositionDiv(x, y) {
+    repositionDiv(evt) {
+
+        evt.preventDefault();
+        evt = evt || window.event;
+        var x = evt.pageX;
+        var y = evt.pageY;
+
         this.setState({
             xCoordinate: x,
             yCoordinate: y,
         });
+
+        console.log(`X: ${x}, Y: ${y}`);
+        console.log(`State X: ${ this.state.xCoordinate }`);
+        console.log(`State Y: ${ this.state.yCoordinate }`)
     }
 
     render(){
         var style = {
             "left": this.state.xCoordinate + "px",
-            "top": this.state.yCoordinate + "px"
+            "top": this.state.yCoordinate + "px",
+            "background-color": "red"
         }
         
         return(
-            <div className="draggable" style={ style }>
+            <div className="draggable-div" draggable onDrag={ (evt) => this.repositionDiv(evt) } style={ style }>
                 Drag me around!
             </div>
         )
