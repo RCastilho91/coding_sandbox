f5801169f84b6f16eb86a07ca19c578163204f28
Rodrigo Castilho <rodrigo.m.castilho@outlook.com>
2020-08-03 18:00:27 -0300

    Adding building blocks for an animated sidebar

---
+++ react-sandbox/src/components/animated-components/side-navigation/SideIcons.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class HomeIcon extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            active: false
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.setState({
+            active: !this.state.active
+        })
+    }
+
+    render(){
+
+        if (this.state.active === true){
+            var elementName = "home-icon-active"
+        } else {
+            var elementName = "home-icon-inactive"
+        }
+
+        return(
+            <div className={ elementName }>
+                Grove St. Home.<p/>
+                At least it was before I fucked everything up.
+            </div>
+        )
+    }
+}
+
+export {
+    HomeIcon
+}
---
+++ react-sandbox/src/components/animated-components/side-navigation/Sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HomeIcon } from './SideIcons';
+
+export default function sideBar() {
+    return(
+        <div className="sidebar">
+            <HomeIcon />
+        </div>
+    )
+}
