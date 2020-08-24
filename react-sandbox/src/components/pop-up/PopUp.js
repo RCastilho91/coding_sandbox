092ac31f2e46efa8557161f0b5c97dec4f77d167
Rodrigo <rodrigo.m.castilho@outlook.com>
2020-08-24 14:39:15 -0300

    Adding fully functional modal component

--- react-sandbox/src/components/pop-up/PopUp.js
+++ react-sandbox/src/components/pop-up/PopUp.js
@@ -1,9 +1,13 @@
 import React from 'react';
-export default function PopUp( handleClose ){
+
+export default function PopUp( props ){
+
+    var handleClosure = props.handleClose
+
     return(
         <div className="modal-container">
             <div className="modal-content">
-                <button onClick={ handleClose }>Close me!</button>
+                <button onClick={ handleClosure }>Close me!</button>
             </div>
         </div>
     );
